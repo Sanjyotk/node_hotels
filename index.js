0e973e241ff7531ad0540a3cec2d312dd81e2e8f
Sanjyotk <sanjyotkotgire@gmail.com>
2024-05-04 16:19:02 +0530

    added logging middleware function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//ALL APP.USE() FUNCTIONS ARE MIDDLE WARE FUNCTIONS
 const express = require('express');
 const app = express();
 const db = require('./db_connect');
@@ -6,6 +7,14 @@ require('dotenv').config();
 //to read the data on client side and send to server
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+//middleware functions: TO ADD MORE FUNCTIONALITY BETWEEN REQ AND RES:LIKE AUNTHENTICATION,LOGGING,MODIFING DATA
+function logRequest(req,res,next){
+    console.log(`[${new Date().toLocaleString()}] Request made to : ${req.originalUrl}`);
+    next(); //to go to server page
+}
+
+app.use(logRequest);
 
 //GET PUT POST DELETE PATCH
 app.get('/', function(req,res){
